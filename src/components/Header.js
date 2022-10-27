5f3ca387291f5214bcf33108db6badbb816151d6
Hikari7 <h.kobe712@gmail.com>
2022-10-27 15:44:59 -0700

    added styling for the nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,30 +5,42 @@ function Header() {
   return (
     <>
       <nav className="px-7 sm:px-4 py-5 rounded">
-        <div className="container flex flex-wrap justify-between items-center mx-auto">
-          <Link to="/">
-            <span className="self-center text-xl whitespace-nowrap dark:text-white opacity-80 font-light">
-              Sleep tight
-            </span>
-          </Link>
-          <button
-            type="button"
-            className="inline-flex items-center p-2  text-sm text-gray-500 rounded-lg md:hidden bg-none"
-            aria-controls="navbar-default"
-            aria-expanded="false"
-          >
-            <span className="sr-only">Open main menu</span>
+        <div className="container flex justify-between items-center mx-auto text-white">
+          <div>
+            <Link to="/">
+              <span className="self-center text-xl whitespace-nowrap font-light cursor-pointer transition hover:duration-700 text-white  opacity-20 hover:opacity-80">
+                Sleep tight
+              </span>
+            </Link>
+          </div>
+          <div className="flex">
             <svg
-              className="w-6 h-6"
-              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
               fill="currentColor"
-              viewBox="0 0 20 20"
-            ></svg>
-          </button>
-          <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-            <ul className="inline-flex items-center p-2  text-sm text-gray-500 rounded-lg md:hidden bg-none">
-              <li>Home</li>
-            </ul>
+              className="bi bi-question-circle mr-4 cursor-pointer transition duration-0 hover:duration-700 opacity-20 hover:opacity-80"
+              viewBox="0 0 16 16"
+            >
+              <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+              <path d="M5.255 5.786a.237.237 0 0 0 .241.247h.825c.138 0 .248-.113.266-.25.09-.656.54-1.134 1.342-1.134.686 0 1.314.343 1.314 1.168 0 .635-.374.927-.965 1.371-.673.489-1.206 1.06-1.168 1.987l.003.217a.25.25 0 0 0 .25.246h.811a.25.25 0 0 0 .25-.25v-.105c0-.718.273-.927 1.01-1.486.609-.463 1.244-.977 1.244-2.056 0-1.511-1.276-2.241-2.673-2.241-1.267 0-2.655.59-2.75 2.286zm1.557 5.763c0 .533.425.927 1.01.927.609 0 1.028-.394 1.028-.927 0-.552-.42-.94-1.029-.94-.584 0-1.009.388-1.009.94z" />
+            </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              className="bi bi-music-note-list cursor-pointer transition duration-0 hover:duration-700 opacity-20 hover:opacity-80"
+              viewBox="0 0 16 16"
+            >
+              <path d="M12 13c0 1.105-1.12 2-2.5 2S7 14.105 7 13s1.12-2 2.5-2 2.5.895 2.5 2z" />
+              <path fill-rule="evenodd" d="M12 3v10h-1V3h1z" />
+              <path d="M11 2.82a1 1 0 0 1 .804-.98l3-.6A1 1 0 0 1 16 2.22V4l-5 1V2.82z" />
+              <path
+                fill-rule="evenodd"
+                d="M0 11.5a.5.5 0 0 1 .5-.5H4a.5.5 0 0 1 0 1H.5a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 .5 7H8a.5.5 0 0 1 0 1H.5a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 .5 3H8a.5.5 0 0 1 0 1H.5a.5.5 0 0 1-.5-.5z"
+              />
+            </svg>
           </div>
         </div>
       </nav>
