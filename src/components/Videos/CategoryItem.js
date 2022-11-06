165a84d462ad861d172907445fb37f0d6fd7d0b6
Hikari7 <h.kobe712@gmail.com>
2022-11-06 10:35:33 -0800

    deleted default btn color

--- src/components/Videos/CategoryItem.js
+++ src/components/Videos/CategoryItem.js
@@ -1,16 +1,17 @@
-import React, { useContext, useState } from "react";
+import React, { useContext } from "react";
 import LightModeContext from "../../contexts/LightMode-context";
+import "../../App.css";
 
 function CategoryItem({ categoryName, handleVideo }) {
   const { lightMode } = useContext(LightModeContext);
-  console.log(lightMode);
+  //console.log(lightMode);
   return (
     <>
       <div className="flex items-center justify-center mt-5 auto-cols-auto">
         <button
           className={`${
             lightMode ? "btn-color-light" : "btn-color"
-          } rounded-2xl p-2 text-sm btn-color hover:bg-indigo-300  text-zinc-500 hover:text-pink-100 transition duration-0 hover:duration-700 ease-in w-4/12 md:w-6/12 h-12`}
+          } rounded-2xl p-2 text-sm hover:bg-indigo-300  text-zinc-500 hover:text-pink-100 transition duration-0 hover:duration-700 ease-in w-4/12 md:w-6/12 h-12`}
           type="button"
           onClick={() => handleVideo(categoryName)}
         >
@@ -23,5 +24,4 @@ function CategoryItem({ categoryName, handleVideo }) {
 
 export default CategoryItem;
 
-//inspectで見ても、toggleはされているけどbtn colorが消えない、消してlightだけ反映させようとしても色が変わらない
-
+//inspectで見ても、toggleはされているけどlightだけ反映させようとしても色が変わらない
